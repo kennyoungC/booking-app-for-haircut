1c04bd101acada3c6aabc36e89190c80411a567e
Kenneth <obikennethe913@gmail.com>
2022-09-21 23:30:35 +0300

    address added

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,17 +1,56 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react"
 
 const Address = (props) => {
   return (
-    <div className="container mx-auto py-16 px-4 grid grid-cols-1 md:grid-cols-2 gap-5 ">
+    <div className="container mx-auto py-16 px-4 gap-5 text-center">
       <div>
-        <h2 className="text-center text-xl font-bold">Address</h2>
-        <address>Universiteto Gatve 8, Akademija, Kaunas. Lithuania</address>
+        <h2 className="text-center text-xl font-bold ">Address</h2>
+        <address className="py-3">
+          Universiteto Gatve 8, Akademija, Kaunas. Lithuania
+        </address>
       </div>
-      <div class="mapouter">
-        <div class="gmap_canvas">
+      <div class="mapouter flex justify-center">
+        <div class="gmap_canvas hidden  lg:block">
+          <iframe
+            width="900"
+            height="600"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=universiteto%20gatve%208%20Akademija&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+        </div>
+        <div class="gmap_canvas hidden  md:block lg:hidden">
           <iframe
             width="600"
             height="500"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=universiteto%20gatve%208%20Akademija&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+        </div>
+        <div class="gmap_canvas hidden sm:block md:hidden">
+          <iframe
+            width="500"
+            height="420"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=universiteto%20gatve%208%20Akademija&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+        </div>
+        <div class="gmap_canvas block sm:hidden">
+          <iframe
+            width="360"
+            height="300"
             id="gmap_canvas"
             src="https://maps.google.com/maps?q=universiteto%20gatve%208%20Akademija&t=&z=13&ie=UTF8&iwloc=&output=embed"
             frameborder="0"
